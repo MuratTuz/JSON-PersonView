402da754fd71dd2a0d90d98d5fa6bd4ef1041b31
Murat <32733938+MuratTuz@users.noreply.github.com>
2017-11-15 22:33:05 +0300

    Json training
    updated version

---
+++ README.md
@@ -0,0 +1,10 @@
+# JS-Training
+## Javascript Excercises
+
+### Notes:
+- The tests are in the file "test/tests.js". You do not need to change any code in the test file!
+- The tests need the functions in the file "js/xxx/index.js" to be implemented. 
+
+#### Usage:
+- This program takes a json object from a textarea and fulfills tables on the page regarding information supplied by the json object
+    
---
+++ js/main.js
@@ -0,0 +1,124 @@
+/**
+ * main.js is the entry point to your program.
+ * You find the program logic, data structures and calls in this file
+ */
+
+"use strict";
+/**
+ * Constand ve global degiskenler buraya gelir.
+ *
+ */
+const personImage = $('#personImage');
+const personName = $('.personName');
+const personDetails = $('.person-details .placeHolder');
+const children = $('.children .placeHolder');
+const friends = $('.friends .placeHolder');
+
+$('#init').on('click', init);
+
+function init() {
+  reset();
+  try{
+    const input = $('#jsonInput').val();
+    person = JSON.parse(input);
+  } catch(e){ }
+
+  personImage.attr('src', getValuesByKey('picture'));
+  personName.html(`${getValuesByKey('firstName')} ${getValuesByKey('lastName')}`)
+  fillPersonDetails();
+  fillChildrenDetails();
+  fillFriendDetails();
+}
+
+function reset(){
+  personImage.attr('src', '');
+  personName.html('');
+  personDetails.html('');
+  friends.html('');
+  children.html('');
+}
+
+function fillPersonDetails() {
+  const email = $('<div>').html(getValuesByKey('email'));
+  const phone = $('<div>').html(getValuesByKey('phone'));
+  personDetails.append(email).append(phone);
+}
+
+function fillChildrenDetails () {
+   const collegues = getValuesByKey('friends');
+   friends.html(createTable(collegues))
+}
+
+function fillFriendDetails(){
+  const kids = getValuesByKey('children');
+  children.html(createTable(kids))
+}
+
+function createTable(items){
+
+  let usableFields = Object.keys(items[0]).filter(a => a !== 'picture');
+
+  const headers = setTableHeaders (usableFields);
+  const rows = setTableDatas(items, usableFields);
+
+  return `
+     <table class="table table-striped col-sm-12">
+        <thead>
+           <tr>
+              ${headers}
+           </tr>
+        </thead>
+        <tbody>
+              ${rows}
+        </tbody>
+     </table>
+  `;
+}
+
+function setTableHeaders (usableFields) {
+  let headers = '';
+  usableFields.forEach(function (key) {
+    headers += `<th>${key}</th>`;
+  });
+  return headers;
+}
+
+function setTableDatas (items, usableFields){
+  let rows = '';
+    
+  items.forEach(function (item){
+    let data = '';
+    let pic = item['picture'];
+    usableFields.forEach(function(field){
+      try{
+
+        if (field === 'address') {
+          const link = "https://www.google.ch/maps/place/"+Object.values(item[field]).join(" ");
+          item[field] = `<a href="${link}" target="blank_">visit me!</a>`;
+        }
+
+        if(moment(item[field], true).isValid()) {
+          item[field] = moment(item[field]).format('DD.MM.YYYY');
+        }
+      } catch(e){
+        console.log('moment is not available');
+      }
+      if (field === 'firstName') {
+        data += `<td>${item[field]}
+        <img class="tool-img"src="${pic}" width="50" height="50">
+        </td>`;
+      }else{
+      data += `<td>${item[field]}</td>`;
+      }
+    })
+
+    rows += `<tr>
+      ${data}
+    </tr>`;
+  });
+  return rows;
+}
+
+function getValuesByKey(key) {
+  return person[key];
+}
